36f9d4a2212955ef5289053dbf4e6fea4e33f67e
aameli-kalkhoran@myseneca.ca <aameli-kalkhoran@myseneca.ca>
2024-02-11 19:23:53 -0500

    lab2.py update

---
+++ lab2.py
@@ -0,0 +1,10 @@
+import sys
+
+def print_script_and_arguments():
+    script_name = sys.argv[0]
+    arguments = sys.argv[1:]
+    print('Script Name:', script_name)
+    print('Script and Variables:', script_name, arguments)
+
+print_script_and_arguments()
+
